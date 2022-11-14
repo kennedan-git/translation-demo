c1f275e5f93ed4ee76ae71392c038f4bfa99ad78
Ken Johnson <kennedan@gmail.com>
2022-11-14 01:00:11 -0500

    empty_cache cuda

--- pyfunc_modules/translation_transformer.py
+++ pyfunc_modules/translation_transformer.py
@@ -51,19 +51,9 @@ class TransformerTranslationModel(mlflow.pyfunc.PythonModel):
         translation = df["content"]
         translation = translation.map(self.translate)
         translations = translation.tolist()
-        #print(encoded_src_txt.shape())
-        #token_id=self._pipe.tokenizer.get_lang_id("pt").asString() 
-        #forced_bos_token_id="128022"
-
-        #debugging code / remove 
-        #test_string = str[0]
-        #encoded_txt = self._pipe.tokenizer(test_string, return_tensors="pt")
-        #encoded_txt = encoded_txt.to(self._pipe.device)
-
-        #generated_tokens = self._pipe.model.generate(**encoded_txt, forced_bos_token_id=self._pipe.tokenizer.get_lang_id("pt"))
-        #text_translations = self._pipe.tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
 
         df_with_translations = pd.DataFrame({"id": ids, "content": texts, "translation": translations})
+        torch.cuda.empty_cache()
         return df_with_translations
 
 def _load_pyfunc(data_path):
