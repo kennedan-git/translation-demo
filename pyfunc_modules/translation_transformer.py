7b0375a5d954866426cd5c4f081b5f96f88e45ae
Ken Johnson <kennedan@gmail.com>
2022-11-14 02:48:42 -0500

    debug

--- pyfunc_modules/translation_transformer.py
+++ pyfunc_modules/translation_transformer.py
@@ -13,12 +13,12 @@ class TransformerTranslationModel(mlflow.pyfunc.PythonModel):
         self._pipe = pipeline
 
     def translate(self, txt):
-        if (len(txt) > 1000): 
+        if (len(txt) > 300): 
             return "Text too long."
         encoded_txt = self._pipe.tokenizer(txt, return_tensors="pt")
         encoded_txt = encoded_txt.to(self._pipe.device)
         generated_tokens = self._pipe.model.generate(**encoded_txt, forced_bos_token_id=self._pipe.tokenizer.get_lang_id("pt"))
-        txt_translation = self._pipe.tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
+        txt_translation = self._pipe.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
         return txt_translation
 
     #def generate(self, **encoded_txt): 
