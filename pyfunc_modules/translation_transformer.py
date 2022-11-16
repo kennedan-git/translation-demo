fd7ad576bcbbe4ebfb9bd77458d72ca55585fdb0
Ken Johnson <kennedan@gmail.com>
2022-11-15 23:36:01 -0500

    debug

--- pyfunc_modules/translation_transformer.py
+++ pyfunc_modules/translation_transformer.py
@@ -25,7 +25,7 @@ class TransformerTranslationModel(mlflow.pyfunc.PythonModel):
         encoded_txt = self._pipe.tokenizer(srctxt, return_tensors="pt", padding=True)
         encoded_txt = encoded_txt.to(self._pipe.device)
         generated_tokens = self._pipe.model.generate(**encoded_txt, forced_bos_token_id=self._pipe.tokenizer.get_lang_id(target_lang))
-        txt_translation = self._pipe.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        txt_translation = self._pipe.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
         return txt_translation
 
     #def generate(self, **encoded_txt): 
